4d5fc12012a7d6d3ba229a42c564f378bc72c8bf
Hovhannisyan11 <garik.hovhannisyan.11@mail.ru>
2023-06-17 23:01:27 +0400

    first

---
+++ js/lab.js
@@ -0,0 +1,29 @@
+$(document).ready(function (){
+  $('.bar').click(function(){
+    $('.drop_down').toggle();
+  })
+
+  $('.p_1').click(function(){
+    $('#p_some1').toggle();
+  })
+
+  $('.p_2').click(function(){
+    $('#p_some2').toggle();
+  })
+
+  $('.x').click(function(){
+    $('.hey').hide()
+  })
+
+  $('.p_3').click(function(){
+    $('.images').toggle();
+  })
+})
+
+
+// function over(elem){ // esel er ashxatum
+//   elem.style.display = 'block'
+// }
+
+
+// out nel bayc piti grei
